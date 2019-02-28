f50491e200540072784aa61c8712d55e8cbf1b90
루트@DATACTR5-W64ADM <cr3ux53c@gmail.com>
2019-02-28 17:43:00 +0900

    공지사항 DB 구축

--- www/admin.py
+++ www/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('공지사항 리스트', {'fields': ['title', 'author', 'summary']}),
+        ('공지 내용', {'fields': ['content', 'html_render', 'image']}),
+    ]
+
+    list_display = ('title', 'summary', 'author', 'date_created')
+
+
 admin.site.register(Maxim)
 admin.site.register(TypeWrite)
+admin.site.register(Post, PostAdmin)
--- www/models.py
+++ www/models.py
@@ -4,17 +4,29 @@ from django.db import models
 
 
 class Maxim(models.Model):
-    content = models.CharField(max_length=500)
-    by_who = models.CharField(max_length=50)
+    content = models.CharField(max_length=500, blank=False)
+    by_who = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
         return '' + self.id.__str__() + ': ' + self.by_who
 
 
 class TypeWrite(models.Model):
-    content = models.CharField(max_length=100)
+    content = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
         return self.content
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=20, null=False, blank=False)
+    summary = models.CharField(max_length=50, null=True, blank=True)
+    author = models.CharField(max_length=20, null=False, blank=False, default='운영진')
+    date_created = models.DateField(null=False, auto_now=True)
+    content = models.TextField(max_length=1024, null=True, blank=False)
+    html_render = models.BooleanField('HTML 렌더링 (non-safe)')
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
