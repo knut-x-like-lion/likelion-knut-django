dfcbd16543285c666ea1d2f2d8dda9307da0fd9c
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-28 12:40:48 +0900

    admin URL 하드코딩 금지

--- likelion_knut_dev/urls.py
+++ likelion_knut_dev/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('www.urls'), name='index'),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
